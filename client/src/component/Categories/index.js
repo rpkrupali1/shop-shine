5212bc10009a25b60d766a930014f2a95b2581e6
tfavro <wetr9902@gmail.com>
2022-05-03 20:05:43 -0500

    added hero to landing page

---
+++ client/src/component/Categories/index.js
@@ -0,0 +1,26 @@
+import React from "react"
+
+const Categories = (props) => {
+    return (
+      <div id='features' className='text-center'>
+      <div className='container'>
+        <div className='col-md-10 col-md-offset-1 section-title'>
+          <h2>Categories</h2>
+        </div>
+        <div className='row'>
+          {props.data
+            ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-xs-6 col-md-3'>
+                  {' '}
+                  <i className={d.icon}></i>
+                  <h3>{d.title}</h3>
+                 
+                </div>
+              ))
+            : 'Loading...'}
+        </div>
+      </div>
+    </div>
+    )
+  }
+  export default Categories;
