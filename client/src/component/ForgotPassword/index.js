bd6ce05f79d47bbf41c3c05f6a9250dbbd5c8828
kpilgulwar <krupalipilgulwar@rocketmortgage.com>
2022-05-06 06:33:41 -0500

    added style to forgot password

--- client/src/component/ForgotPassword/index.js
+++ client/src/component/ForgotPassword/index.js
@@ -1,25 +1,29 @@
 import React, { useState } from "react";
-import {ImConfused} from "react-icons/im"
+import { ImConfused } from "react-icons/im";
+import "../../assets/styles/successCancel.css";
 
 function ForgotPassword() {
   return (
-    <div className="wrapper">
-      <p className="icon">
-        <ImConfused />
-      </p>
-      <h2>FORGOT PASSWORD</h2>
-      <p className="message">
-        Please enter your email address below to receive a password reset link.
-      </p>
-      <form>
-        <div>
-          <label htmlFor="email">
-            Email<span>*</span>
-          </label>
-          <input id="email" placeholder="youremail@test.com" type="email" />
-        </div>
-        <button className="resetBtn">RESET MY PASSWORD</button>
-      </form>
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="danger-icon">
+          <ImConfused />
+        </p>
+        <h2>FORGOT PASSWORD</h2>
+        <p className="email-msg">
+          Please enter your email address below to receive a password reset
+          link.
+        </p>
+        <form>
+          <div className="resetEmail">
+            <label htmlFor="email" className="email">
+              Email<span className="error-text">*</span>
+            </label>
+            <input id="email" placeholder="youremail@test.com" type="email" />
+          </div>
+          <button className="resetbtn">RESET MY PASSWORD</button>
+        </form>
+      </div>
     </div>
   );
 }
