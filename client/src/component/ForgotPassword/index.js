8bd9ab0ba7cf036a7024d7a363216e098ccb7157
kpilgulwar <krupalipilgulwar@rocketmortgage.com>
2022-05-06 06:50:05 -0500

    added handle change

--- client/src/component/ForgotPassword/index.js
+++ client/src/component/ForgotPassword/index.js
@@ -3,6 +3,22 @@ import { ImConfused } from "react-icons/im";
 import "../../assets/styles/successCancel.css";
 
 function ForgotPassword() {
+  const [email, setEmail] = useState("");
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    return (
+      <div>
+        <p>Please check your email at {email}</p>
+      </div>
+    );
+  };
+
+  const handleChange = (event) => {
+    const emailVal = event.target.value;
+    setEmail(emailVal);
+  };
+
   return (
     <div className="success-wrapper">
       <div className="success">
@@ -14,12 +30,17 @@ function ForgotPassword() {
           Please enter your email address below to receive a password reset
           link.
         </p>
-        <form>
+        <form onSubmit={handleFormSubmit}>
           <div className="resetEmail">
             <label htmlFor="email" className="email">
               Email<span className="error-text">*</span>
             </label>
-            <input id="email" placeholder="youremail@test.com" type="email" />
+            <input
+              id="email"
+              placeholder="youremail@test.com"
+              type="email"
+              onChange={handleChange}
+            />
           </div>
           <button className="resetbtn">RESET MY PASSWORD</button>
         </form>
