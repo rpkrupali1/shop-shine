0edf144ec3671ce672b25aebe7f070ea7cbb502b
rpkrupali1 <rpkrupali@gmail.com>
2022-05-07 13:12:45 -0500

    reset password

--- client/src/component/ForgotPassword/index.js
+++ client/src/component/ForgotPassword/index.js
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { ImConfused } from "react-icons/im";
+import { FiCheckCircle } from "react-icons/fi";
 import "../../assets/styles/successCancel.css";
 
 function ForgotPassword() {
   const [email, setEmail] = useState("");
   const [errorMessage, setErrorMessage] = useState();
   const [btnClick, setBtnClick] = useState(false);
+  const [header, setHeader] = useState("FORGOT PASSWORD");
+  const [desc, setDesc] = useState(
+    "Please enter your email address below to receive a password reset link."
+  );
+  const [iconClass, setIconClass] = useState("danger-icon");
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
     if (email) {
       setBtnClick(true);
+      setHeader("PASSWORD HAS RESET");
+      setDesc(
+        `Please check your email, we have sent you an email for password reset at ${email}`
+      );
+      setIconClass("reset-icon");
     } else setErrorMessage("Please enter valid email id");
   };
 
@@ -25,41 +36,31 @@ function ForgotPassword() {
   return (
     <div className="success-wrapper">
       <div className="success">
-        <p className="danger-icon">
-          <ImConfused />
-          {/* {btnClicked === true && <h2>Forgot Password</h2>} */}
+        <p className={iconClass}>
+          {btnClick === false ? <ImConfused /> : <FiCheckCircle />}
         </p>
-        <h2>FORGOT PASSWORD</h2>
-        <p className="email-msg">
-          Please enter your email address below to receive a password reset
-          link.
-        </p>
-        <form onSubmit={handleFormSubmit}>
-          <div className="resetEmail">
-            <label htmlFor="email" className="email">
-              Email<span className="error-text">*</span>
-            </label>
-            <input
-              id="email"
-              placeholder="youremail@test.com"
-              type="email"
-              onChange={handleChange}
-            />
-          </div>
-          {errorMessage && (
-            <div>
-              <p className="error-text">{errorMessage}</p>
+        <h2>{header}</h2>
+        <p className="email-msg">{desc}</p>
+        {!btnClick && (
+          <form onSubmit={handleFormSubmit}>
+            <div className="resetEmail">
+              <label htmlFor="email" className="email">
+                Email<span className="error-text">*</span>
+              </label>
+              <input
+                id="email"
+                placeholder="youremail@test.com"
+                type="email"
+                onChange={handleChange}
+              />
             </div>
-          )}
-          <button className="resetbtn">RESET MY PASSWORD</button>
-        </form>
-        {btnClick && (
-          <div>
-            <p>
-              Please check your email, we have sent you an email for password
-              reset {email}
-            </p>
-          </div>
+            {errorMessage && (
+              <div>
+                <p className="error-text">{errorMessage}</p>
+              </div>
+            )}
+            <button className="resetbtn">RESET MY PASSWORD</button>
+          </form>
         )}
       </div>
     </div>
